3621496e1b720c77e700eb2f2b80ce0c55128e82
Gerecske <szil.gergo9@gmail.com>
2020-09-10 21:38:51 +0200

    Ősz

---
+++ VeregGyak/Assets/Semmike.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Semmike : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Szeretem az őszt");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
